5bdf1acbe1f22402edfdf7dc95a44d697c1b4775
Mahdi <mahdimeyghani02@gmail.com>
2024-06-18 17:08:30 +0330

    Scrap the page source from the url

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 import requests
 import selectorlib
+
+URL = "http://programmer100.pythonanywhere.com/tours/"
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) '
+                  'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
+}
+
+
+def scraper(url):
+    """Scraps a web page"""
+    response = requests.get(url, headers=HEADERS)
+    source = response.text
+
+    return source
+
+
+if __name__ == "__main__":
+    print(scraper(URL))
