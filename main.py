c0943a6ddba8897c6187792fdd1cc772c40acba5
Mahdi <mahdimeyghani02@gmail.com>
2024-06-20 14:58:26 +0330

    Create a read tours data function from database

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ import smtplib
 import ssl
 import os
 import time
+import sqlite3
 
 URL = "http://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) '
                   'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
 }
+CONN = sqlite3.connect('tours.db')
 
 
 def scrape_tours(url):
@@ -45,18 +47,31 @@ def send_email(message):
     print("Email was sent successfully!")
 
 
-def store_tours(tour_text):
+def store_tours_file(tour_text):
     """Save the tours info into a file"""
     with open("tours.txt", "a") as tours_file:
         tours_file.write(tour_text + "\n")
 
 
-def read_tours():
+def read_tours_file():
     """Read the tours info from a file and return it as a string"""
     with open("tours.txt", "r") as tours_file:
         tours_text = tours_file.read()
 
     return tours_text
+
+
+def read_tours_db(tour_text):
+    """Read the tours info from database and return it as a list of tuples"""
+    row = tour_text.split(", ")
+    bandName, city, date = row
+
+    cursor = CONN.cursor()
+    cursor.execute("SELECT * FROM events WHERE bandName = ? AND city = ? AND date = ?",
+                   (bandName, city, date))
+    exists = cursor.fetchall()
+
+    return exists
 
 
 if __name__ == "__main__":
@@ -65,10 +80,11 @@ if __name__ == "__main__":
         tours = extract_tours(source_text)
         print(tours)
 
-        all_tours = read_tours()
-
-        if tours != "No upcoming tours" and tours not in all_tours:
-            store_tours(tours)
-            send_email(f"Subject: Music Tour\n\nHey, new event was found on:\n{tours}")
+        if tours != "No upcoming tours":
+            exist = read_tours_db(tour_text=tours)
+            print(exist)
+            if not exist:
+                store_tours_file(tours)
+                send_email(f"Subject: Music Tour\n\nHey, new event was found on:\n{tours}")
 
         time.sleep(2)
